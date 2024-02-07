3ed524d737c2b1d5297e3157507820bc8a71c3d9
Miruna <miruna_haidu@yahoo.com>
2024-02-07 16:34:51 +0200

    fix router

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.render(
+  <BrowserRouter basename="/age-calculator">
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
+  document.getElementById('root')
 )
